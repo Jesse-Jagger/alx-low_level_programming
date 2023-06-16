dee44086b4f8625c6a1a3df5ed5c37f7bf45e2c4
Jesse-Jagger <jessejackson.055@gmail.com>
2023-06-16 09:15:34 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,12 +2,12 @@
 /**
 * main - Main entry point
 *
-* description: Use the function printf
+* Description: Use the function printf
 *
-* return: Always 0 (Success)
+* Return: Always 0 (Success)
 */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,n/");
+	printf("with proper grammar, but the outcome is a piece of art,");
 	return (0);
 }
