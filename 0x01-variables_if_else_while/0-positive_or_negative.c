1e73b46f5346267607ce247b1306fc9564cbd9f4
Jesse-Jagger <jessejackson.055@gmail.com>
2023-06-16 21:01:44 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -18,13 +18,13 @@ int main(void)
 	{
 		printf("%d is positive\n", n);
 	}
-	else if
+	else if (n == 0)
 	{
 		printf("%d is zero\n", n);
 	}
 	else
 	{
-		printf("%d is negative\n", n);
+		printf("%d is positive\n", n);
 	}
 	return (0);
 }
