ba2373443ffbd7f49faf0190e430e427ebffacfe
Jesse-Jagger <jessejackson.055@gmail.com>
2023-06-18 02:35:12 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - lowercase alphabet in reverse
+ *
+ * Description: print lowercase alphabet in reverse
+ *
+ * Return: always 0 (success)
+*/
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	while (n >= 122)
+	{
+		putchar(n);
+		--n;
+	}
+	putchar('\n');
+	return (0);
+}
