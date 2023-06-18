cf88fa3bcaa34e543ec9c6effe253699c4e4efa3
Jesse-Jagger <jessejackson.055@gmail.com>
2023-06-18 11:02:05 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - combinations of single-digit numbers
+ *
+ * Description: prints all possible combinations of single-digit numbers
+ *
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+	int i;
+
+	for (i == 48; i <= 57; i++)
+	{
+		putchar(i);
+		if (i == 57)
+		{	
+			continue;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
