dcab5eef46aa9731b1eb1180bfc6b5fcd901e778
Jesse-Jagger <jessejackson.055@gmail.com>
2023-06-18 22:51:14 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - combinations of two digits
+ *
+ * Description: prints all possible different combinations of two digits.
+ *
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+	int a;
+	int b;
+	int c;
+
+	for (a = 48; a < 57; a++)
+	{
+		for (b = 49; b < 57; b++)
+		{
+			for (c = 50; c <= 57; c++)
+			{
+				if (c > b && b > a)
+				{
+					putchar(a);
+					putchar(b);
+					putchar(c);
+					if (a != 55 || b != 56)
+					{
+					putchar(',');
+					putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
