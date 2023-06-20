67c57c27a88f4ef593b9b06668e1eaeaed71b892
Jesse-Jagger <jessejackson.055@gmail.com>
2023-06-19 20:06:32 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,12 @@
 /**
 * main - Main entry point
 *
-* Description: Use the function printf
+* Description: program that prints exactly standard error.
 *
 * Return: Always 1 (Success)
 */
 int main(void)
 {
-	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
