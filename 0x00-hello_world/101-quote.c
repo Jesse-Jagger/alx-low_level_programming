ce3848af84183e9562e02dbb232912ee4a6b48c2
Jesse-Jagger <jessejackson.055@gmail.com>
2023-06-19 19:51:33 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - Main entry point
+*
+* Description: Use the function printf
+*
+* Return: Always 1 (Success)
+*/
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
