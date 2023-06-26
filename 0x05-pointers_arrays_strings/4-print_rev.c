de148143f15430380bab2fe9aacd948ef535b537
Jesse-Jagger <jessejackson.055@gmail.com>
2023-06-26 15:45:10 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_rev - imprime en reversa
+ * @s: string
+ * return: 0
+ */
+void print_rev(char *s)
+{
+	char *s[] = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+	{
+		printf(*s)
+		* s--
+	}
