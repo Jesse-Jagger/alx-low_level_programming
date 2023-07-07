1ec4c08095e8f2a2f5a6d890aef4f2a23ed0122c
Jesse-Jagger <jessejackson.055@gmail.com>
2023-07-07 16:34:56 -0700

    strncat.c

---
+++ 0x09-static_libraries/puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - A program that print a line with the function puts
+ *
+ * Description: Print a line with the function puts
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
