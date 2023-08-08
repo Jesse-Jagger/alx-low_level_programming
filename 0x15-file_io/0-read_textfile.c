efb97ab7f8dfcf07f59914efc9165455566a2ecb
Jesse-Jagger <jessejackson.055@gmail.com>
2023-08-08 11:18:36 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,17 +13,17 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	char *tuf;
 	ssize_t ad;
-	ssize_t v;
+	ssize_t w;
 	ssize_t t;
 
 	ad = open(filename, O_RDONLY);
 	if (ad == -1)
 		return (0);
 	tuf = malloc(sizeof(char) * letters);
-	v = read(ad, tuf, letters);
-	t = write(STDOUT_FILENO, tuf, t);
+	t = read(ad, tuf, letters);
+	w = write(STDOUT_FILENO, tuf, t);
 
 	free(tuf);
 	close(ad);
-	return (v);
+	return (w);
 }
